c55a18a932942889fa7eb902be13459c2d2d3bbe
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-12-08 21:51:53 +0300

    add mapping params

--- src/main/java/com/dmitry/web/controllers/HelloWorldController.java
+++ src/main/java/com/dmitry/web/controllers/HelloWorldController.java
@@ -1,14 +1,25 @@
 package com.dmitry.web.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping(value = {"/home"})
 public class HelloWorldController {
-    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
-    public String home() {
+//    @RequestMapping(value = {"first", "third"}, method = RequestMethod.GET, params = "!myParam")
+    @GetMapping(value = "first")
+    @ResponseBody
+    public String home1() {
       //  return "/WEB-INF/views/home.jsp";
-        return "home";
+        return "home1";
     }
+
+    //@PostMapping
+    //или
+    @RequestMapping(value = "second", method = RequestMethod.POST)
+    @ResponseBody
+    public String home2() {
+        return "home2";
+    }
+
 }
