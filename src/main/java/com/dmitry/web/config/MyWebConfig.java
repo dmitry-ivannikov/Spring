1247ec4d52a5795638e9bbe5dedfef1e47a286da
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-12-08 21:10:30 +0300

    add resolver

--- src/main/java/com/dmitry/web/config/MyWebConfig.java
+++ src/main/java/com/dmitry/web/config/MyWebConfig.java
@@ -1,9 +1,19 @@
 package com.dmitry.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("com.dmitry.web.controllers")
 public class MyWebConfig {
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
--- src/main/java/com/dmitry/web/controllers/HelloWorldController.java
+++ src/main/java/com/dmitry/web/controllers/HelloWorldController.java
@@ -3,11 +3,12 @@ package com.dmitry.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
     @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
-    @ResponseBody
-    public String home() { return "hello world"; }
+    public String home() {
+      //  return "/WEB-INF/views/home.jsp";
+        return "home";
+    }
 }
