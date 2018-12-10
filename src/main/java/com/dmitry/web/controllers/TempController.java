ec2f88ed363670e3bb69df535d2b352f6bd90c12
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-12-10 17:55:13 +0300

    add mvc PathVariable 39 7

--- src/main/java/com/dmitry/web/controllers/TempController.java
+++ src/main/java/com/dmitry/web/controllers/TempController.java
@@ -1,20 +1,27 @@
 package com.dmitry.web.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/reqParam/{one}")
 public class TempController {
-    @GetMapping("reqParam")
+    @GetMapping("/reqParam/{one}/two/{three}")
     @ResponseBody
-    public String getParams(@RequestParam("paramPage") String first,
-                            @RequestParam(required = false, defaultValue = "nuPrivet") String second) {
-//        http://localhost:8080/spring/reqParam?paramPage=bugaga (значение не проставленно, но приходит дефолтное значение)
-        return first + second;
-//        http://localhost:8080/spring/reqParam?paramPage=bugaga
-//        http://localhost:8080/spring/reqParam?paramPage=bugaga&second=2
-
+    public String getParams(@PathVariable("one") String one, @PathVariable int three) {
+        return one + three;
+//        http://localhost:8080/spring/reqParam/first/two/3
     }
+
+
+
+//    @GetMapping("reqParam")
+//    @ResponseBody
+//    public String getParams(@RequestParam("paramPage") String first,
+//                            @RequestParam(required = false, defaultValue = "nuPrivet") String second) {
+////        http://localhost:8080/spring/reqParam?paramPage=bugaga (значение не проставленно, но приходит дефолтное значение)
+//        return first + second;
+//    }
+
+
 }
