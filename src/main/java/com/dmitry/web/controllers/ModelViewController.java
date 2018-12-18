0227aea1173d7e2d799710d382383cfa6f6e4f55
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-12-18 11:06:23 +0300

    add mvc ModelAndView 45 13

---
+++ src/main/java/com/dmitry/web/controllers/ModelViewController.java
@@ -0,0 +1,15 @@
+package com.dmitry.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ModelViewController {
+    @GetMapping("modelView")
+    public ModelAndView getMethod() {
+        ModelAndView modelAndView = new ModelAndView("modelView");
+        modelAndView.addObject("userName", "dima");
+        return modelAndView;
+    }
+}
