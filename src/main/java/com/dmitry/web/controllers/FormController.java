02a5e1dea057dd7a303a75095f2b9f73bf6ddc43
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-12-18 10:56:45 +0300

    add mvc Redirect, forward, session и flash attributes 44 12

--- src/main/java/com/dmitry/web/controllers/FormController.java
+++ src/main/java/com/dmitry/web/controllers/FormController.java
@@ -3,6 +3,10 @@ package com.dmitry.web.controllers;
 import com.dmitry.web.repo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
+
 
 @Controller
 public class FormController {
@@ -19,8 +23,15 @@ public class FormController {
 //    }
 
     @PostMapping("/form")
-    @ResponseBody
-    public String getFormData(User user){
-        return  user.getName() + " " + user.getAge();
+//    @ResponseBody
+    public String getFormData
+//            (User user, HttpSession session)
+    (User user, RedirectAttributes model)
+    {
+//        return  user.getName() + " " + user.getAge();
+//        session.setAttribute("user", user);
+        model.addFlashAttribute("user", user);
+        return "redirect:/spring/redirTo";
+//        return "forward:/spring/redirTo";
     }
 }
