84e6b249cb3dd2fe1ed5bb424dfbc421a9c62c68
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2019-01-07 15:31:46 +0300

    Spring 59: Data 8: Query object

---
+++ src/main/java/com/dmitry/entities/MyTable.java
@@ -0,0 +1,27 @@
+package com.dmitry.entities;
+
+public class MyTable {
+    int id;
+    String name;
+
+    public MyTable(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/dmitry/web/controllers/DbController.java
+++ src/main/java/com/dmitry/web/controllers/DbController.java
@@ -1,10 +1,15 @@
 package com.dmitry.web.controllers;
 
+import com.dmitry.entities.MyTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Controller
 public class DbController {
@@ -13,12 +18,29 @@ public class DbController {
 
     @GetMapping("createTable")
     @ResponseBody
-    public String createTable(){
+    public void createTable(){
         jdbcOperations.execute("CREATE TABLE IF NOT EXISTS MyTable (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(30), lname VARCHAR(30));");
         jdbcOperations.update("INSERT into MyTable (fname) VALUES ('first book')");
         jdbcOperations.update("update MyTable set fname = 'second book' where id = ?", 1);
 
         //        jdbcOperations.execute("DROP TABLE MyTable");
-        return "table was created";
+//        MyTable myTable = jdbcOperations.queryForObject("select * from MyTable where id = ?",  new MyRowMapper(), 1);
+
+        MyTable myTable = jdbcOperations.queryForObject("select * from MyTable where id = ?",
+                (rs,i) -> {return
+                        new MyTable(
+                                rs.getInt("id"),
+                                rs.getString("fname")
+                                    );
+                            }, 1);
+
+        System.out.println(myTable.getId());
+        System.out.println(myTable.getName());
     }
+//    static class MyRowMapper implements RowMapper<MyTable>{
+//
+//        public MyTable mapRow(ResultSet resultSet, int i) throws SQLException {
+//            return new MyTable(resultSet.getInt("id"), resultSet.getString("fname"));
+//        }
+//    }
 }
