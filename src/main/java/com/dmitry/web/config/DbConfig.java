762ef1cf29e63a4cc80771cea29df9d0c86abb3a
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2019-04-11 13:15:39 +0300

    Spring 60: Data 9: Named parametrs

--- src/main/java/com/dmitry/web/config/DbConfig.java
+++ src/main/java/com/dmitry/web/config/DbConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -56,6 +57,11 @@ public class DbConfig {
     @Bean
     public JdbcTemplate getJdbcTemplate(DataSource dataSource){
         return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
     }
 
 }
--- src/main/java/com/dmitry/web/controllers/DbController.java
+++ src/main/java/com/dmitry/web/controllers/DbController.java
@@ -4,24 +4,32 @@ import com.dmitry.entities.MyTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class DbController {
     @Autowired
     JdbcOperations jdbcOperations;
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
 
     @GetMapping("createTable")
     @ResponseBody
     public void createTable(){
         jdbcOperations.execute("CREATE TABLE IF NOT EXISTS MyTable (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(30), lname VARCHAR(30));");
         jdbcOperations.update("INSERT into MyTable (fname) VALUES ('first book')");
-        jdbcOperations.update("update MyTable set fname = 'second book' where id = ?", 1);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 1);
+        namedParameterJdbcTemplate.update("update MyTable set fname = 'second book' where id = :id", map);
 
         //        jdbcOperations.execute("DROP TABLE MyTable");
 //        MyTable myTable = jdbcOperations.queryForObject("select * from MyTable where id = ?",  new MyRowMapper(), 1);
